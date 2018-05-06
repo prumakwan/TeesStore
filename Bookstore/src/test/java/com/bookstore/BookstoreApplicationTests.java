da0c1a6fa891c4604900cc7ea0fb82bb32ce49e7
Pruthviraj Makwana <33934404+prumakwan@users.noreply.github.com>
2018-05-06 12:53:36 +0530

    updated
    Update

--- Bookstore/src/test/java/com/bookstore/BookstoreApplicationTests.java
+++ Bookstore/src/test/java/com/bookstore/BookstoreApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//strat from here
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BookstoreApplicationTests {
